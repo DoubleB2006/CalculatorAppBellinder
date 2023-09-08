66aa9a27d49edef42ce73d5e20fe743839624a29
DoubleB2006 <113694379+DoubleB2006@users.noreply.github.com>
2023-09-08 08:19:09 -0500

    9/8
    I am fixing the style to make it look nice
    I am struggling to get the button and text sizes right  but I am getting the functionality to work

--- app/src/main/java/com/example/calculatorappbellinder/MainActivity.java
+++ app/src/main/java/com/example/calculatorappbellinder/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.calculatorappbellinder;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,6 +12,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+    }
+    public void addNums(View v){
+        EditText num1 = findViewById(R.id.Number1);
+        EditText num2 = findViewById(R.id.Number2);
+        EditText num3 = findViewById(R.id.Number3);
 
 
     }
