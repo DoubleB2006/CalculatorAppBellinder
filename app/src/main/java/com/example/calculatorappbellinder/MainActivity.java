41721d735ef89526f4e8efbce90cd4a0c6335386
DoubleB2006 <113694379+DoubleB2006@users.noreply.github.com>
2023-09-12 08:18:56 -0500

    9/12
    I have everything working now I just need to turn in my project I even have extra functions to square the values

--- app/src/main/java/com/example/calculatorappbellinder/MainActivity.java
+++ app/src/main/java/com/example/calculatorappbellinder/MainActivity.java
@@ -15,6 +15,9 @@ public class MainActivity extends AppCompatActivity {
 
 
     }
+    /* website where I found Parse Int
+    https://sentry.io/answers/how-do-i-convert-a-string-to-an-int-in-java/#:~:text=The%20two%20easiest%20ways%20to,valueOf()%20.
+     */
     public void addNums(View v){
         EditText n1 = findViewById(R.id.Number1);
         EditText n2 = findViewById(R.id.Number2);
@@ -43,14 +46,14 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
-    public void muliplyNums(View v){
+    public void multiplyNums(View v){
         EditText n1 = findViewById(R.id.Number1);
         EditText n2 = findViewById(R.id.Number2);
         EditText n3 = findViewById(R.id.Number3);
 
         int num1 = Integer.parseInt(n1.getText().toString());
         int num2 = Integer.parseInt(n2.getText().toString());
-        int num3 = num1 * num2;
+        int num3 = num1*num2;
 
         n3.setText(" " + num3);
 
@@ -65,6 +68,33 @@ public class MainActivity extends AppCompatActivity {
         int num1 = Integer.parseInt(n1.getText().toString());
         int num2 = Integer.parseInt(n2.getText().toString());
         int num3 = num1 / num2;
+
+        n3.setText(" " + num3);
+
+
+    }
+    public void squareFirstNum(View v){
+        EditText n1 = findViewById(R.id.Number1);
+        EditText n2 = findViewById(R.id.Number2);
+        EditText n3 = findViewById(R.id.Number3);
+
+        int num1 = Integer.parseInt(n1.getText().toString());
+        int num2 = Integer.parseInt(n2.getText().toString());
+        int num3 = num1*num1;
+
+        n3.setText(" " + num3);
+
+
+    }
+
+    public void squareSecondNum(View v){
+        EditText n1 = findViewById(R.id.Number1);
+        EditText n2 = findViewById(R.id.Number2);
+        EditText n3 = findViewById(R.id.Number3);
+
+        int num1 = Integer.parseInt(n1.getText().toString());
+        int num2 = Integer.parseInt(n2.getText().toString());
+        int num3 = num2*num2;
 
         n3.setText(" " + num3);
 
